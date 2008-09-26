bed11e3ac1f20e8bb909f0c5140e3e329d6910e7
opennetadmin <matt@opennetadmin.com>
2008-09-26 20:06:37 +0000

    MP: new sys configuration editor.  needs a tone of work still

--- install/4-to-5.sql
+++ install/4-to-5.sql
@@ -11,3 +11,9 @@ ALTER TABLE `dcm_module_list` DROP `id`;
 INSERT INTO `dcm_module_list` (`name` , `description` , `file`)
 VALUES ('add_module', 'Register a new DCM module', 'get_module_list.inc.php');
 
+-- this is subject to change.. may not want to deploy this part yet
+ALTER TABLE `sys_config` ADD `field_validation_rule` TEXT NOT NULL COMMENT 'Regular expression to validate content of the value column';
+ALTER TABLE `sys_config` ADD `editable` TINYINT( 1 ) NOT NULL COMMENT 'Can this record be edited?';
+ALTER TABLE `sys_config` ADD `deleteable` TINYINT( 1 ) NOT NULL COMMENT 'Can this record be deleted?';
+
+-- set up the  new sys_config user
