51d7f5ce4faa029d455ddf89b0cc2fe01b553e38
opennetadmin <matt@opennetadmin.com>
2008-11-20 17:20:36 +0000

    MP: added location variable to host action substitution.  helps when you have things like nagios.boi.example.com or nagios.lax.example.com.  this example assumes location reference is also part of the domain structure.

--- www/workspace_plugins/builtin/host_actions/config.inc.php
+++ www/workspace_plugins/builtin/host_actions/config.inc.php
@@ -1,12 +1,15 @@
 <?php
 // This section defines host actions. If you leave the url blank it will not show the option in the list
-// You can use %fqdn and %ip as substitutions in the url for the host being displayed
+// You can use %fqdn, %ip and %loc as substitutions in the url for the host being displayed
 // You can specify a tooltip title for the option, otherwise it defaults to the hostaction name "Telnet" "Splunk" etc
 // These will be listed in the order specified here.
+// %loc = the "reference" value from the hosts location
+// %fqdn = the fully qualified primary name of this host
+// %ip = the IP address of the primary interface for this host
 
 
 //EXAMPLE:$conf[$modulename]['LINK_TITLE']['url'] = "http://something";
 $conf[$modulename]['Splunk']['url'] = "https://splunk.example.com:8001/?events/?eventspage=1&num=10&q=%fqdn";
-$conf[$modulename]['Cacti Graph']['url'] = "https://cacti.example.com/cacti/graph_view.php?action=tree&name=%fqdn";
+$conf[$modulename]['Cacti Graph']['url'] = "https://cacti.%loc.example.com/cacti/graph_view.php?action=tree&name=%fqdn";
 $conf[$modulename]['Wiki Page']['url'] = "https://wiki.example.com/dokuwiki/network/servers/%fqdn";
 ?>
--- www/workspace_plugins/builtin/host_actions/main.inc.php
+++ www/workspace_plugins/builtin/host_actions/main.inc.php
@@ -1,5 +1,9 @@
 <?php
 
+$rec_content = print_r($record, true);
+$debug_display = 1;  // Set this to 1 to see contents of the $record array
+
+
 // Load any user specific entries and add them to the list
 // If you re-define an existing entry, it will override the default
 $mod_conf="{$base}/local/config/{$modulename}.config.php";
@@ -9,23 +13,34 @@ if (file_exists($mod_conf)) { require_once($mod_conf); }
 $title_left_html = 'Host Actions';
 
 
-
 $title_right_html .= <<<EOL
                 <img title="Edit plugin configuration" onClick="" src="{$images}/silk/plugin_edit.png" border="0">
 EOL;
 
 
+if ($debug_display) {
+    $modbodyhtml .= <<<EOL
+        <pre>{$rec_content}</pre>
+EOL;
+}
 
-
+// If there is a location id then get the info so we can replace it later
+if ($record['location_id'] > 0) {
+    // TODO: maybe do some checks if we dont find a location but ohhhh well for now.
+    list($status, $rows, $location) = ona_get_location_record(array('id' => $record['location_id']));
+}
 
 // Build hostaction list from the $conf['hostaction'] array
 foreach ($conf[$modulename] as $hostaction=>$hval) {
     // Skip the module title entry
     // Use the title if there is one, otherwise just use the arrayname
     $hval['title'] = ($hval['title']) ? $hval['title'] : $hostaction;
-    // Substitute %fqdn and %ip
+    // Substitute %fqdn and %ip and %loc
     $hval['url'] = str_replace('%fqdn', $record['fqdn'], $hval['url']);
     $hval['url'] = str_replace('%ip', $record['ip_address'], $hval['url']);
+    $hval['url'] = str_replace('%loc', $location['reference'], $hval['url']);
+
+
 
     // If the URL has data in it, print.
     // TODO: MDP, maybe offer an $hval['icon'] option to use a different icon specified in the $conf['hostaction']['Name']['icon'] variable
