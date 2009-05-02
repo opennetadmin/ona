1af2b2122ee647775a8e0be79c4dc3dbe481eca7
opennetadmin <matt@opennetadmin.com>
2009-05-02 18:54:32 +0000

    MP: quick add of a new module for this release

--- install/5-to-6.sql
+++ install/5-to-6.sql
@@ -1,3 +1,6 @@
 ALTER TABLE `blocks` CHANGE `notes` `notes` VARCHAR( 255 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL;  
 
 INSERT INTO `sequences` VALUES ('configuration_types',5);
+
+INSERT INTO `dcm_module_list` VALUES ('custom_attribute_display', 'Display information about custom attributes', 'ona/custom_attribute.inc.php');
+
