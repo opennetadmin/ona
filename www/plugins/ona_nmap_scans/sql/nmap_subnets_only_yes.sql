c481a1d9216d62b29f9c73a6acfdc4bfb15dfc86
opennetadmin <matt@opennetadmin.com>
2010-05-27 21:49:46 +0000

    MP: at the request of Psyber I have made the default behavior of ona_nmap_scan only go after subnets marked as Y.  If you want any subnet exept those marked with N use the -a option.

--- www/plugins/ona_nmap_scans/sql/nmap_subnets_only_yes.sql
+++ www/plugins/ona_nmap_scans/sql/nmap_subnets_only_yes.sql
@@ -1,23 +1,24 @@
--- Lists all subnets with CIDR mask, use for nmap_ona_audit.php
+-- Lists only Y flagged subnets with CIDR mask, use for nmap_ona_audit.php
 --
 -- LONG_DESC:
--- 	This query returns all subnets from the database in
+-- 	This query returns ONLY subnets from the database that have
+--      the "nmap_scan" custom attribute set to "Y". The output will be in
 --	network-ip/cidr notation.  It will NOT return subnets that
 --	have the "nmap_scan" custom attribute set to "N".
 --
 --	The intent is that this list will be used as a seed for
---	doing nmap scans of all subnets in the database.
---      If you wish to find ONLY subnets that are flagged as "Y"
---      then use the nmap_subnets_only_yes.sql file instead.
+--	doing nmap scans of only specified subnets in the database.
+--      If you wish to find ANY subnet that is not flagged as "N"
+--      then use the nmap_subnets.sql file instead.
 --
 
 select CONCAT(INET_NTOA(ip_addr),'/',32-log2((4294967296-ip_mask))) net
 from subnets 
-where id not in (
+where id in (
         SELECT table_id_ref
         FROM custom_attributes
         WHERE table_name_ref = 'subnets'
-        AND value = 'N'
+        AND value = 'Y'
         AND custom_attribute_type_id = (
                 SELECT id
                 FROM custom_attribute_types
