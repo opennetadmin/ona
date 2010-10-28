e4ef0feef20577bd02ce6e2267d432217b57035e
opennetadmin <matt@opennetadmin.com>
2010-10-28 22:41:58 +0000

    MP: added proxy option for version check module.. could use more testing

--- www/workspace_plugins/builtin/desktop_versioncheck/main.inc.php
+++ www/workspace_plugins/builtin/desktop_versioncheck/main.inc.php
@@ -1,28 +1,48 @@
 <?php
+// The following checks with the opennetadmin server to see what the most current version is.
+// It will do this each time the interface is opened so the traffic should be very minimal.
+
 
 $title_right_html = '';
 $title_left_html  = '';
 $modbodyhtml = '';
 
-// if this is a display host screen then go ahead and make a puppet facts window
+// Display only on the desktop
 if ($extravars['window_name'] == 'html_desktop') {
-
-    // The following checks with the opennetadmin server to see what the most current version is.
-    // It will do this each time the interface is opened so the traffic should be very minimal.
     // Dont perform a version check if the user has requested not to
     if (!$conf['skip_version_check']) {
-        @ini_set('user_agent',$_SERVER['HTTP_USER_AGENT']."-----".$conf['version']);
-        //$onachkserver = @gethostbynamel('opennetadmin.com');
-        $onachkserver = 'opennetadmin.com';
-        if ($onachkserver) {
+        //@ini_set('user_agent',$_SERVER['HTTP_USER_AGENT']."-----".$conf['version']);
+
+        // Define the URL we use to check what version is the latest release
+        $ona_check_version_url = 'http://opennetadmin.com/check_version.php';
+
+        if ($ona_check_version_url) {
+            $stream_opts = array(
+                'http'=> array(
+                    'method'=>   'GET',
+                    'user_agent'=>  $_SERVER['HTTP_USER_AGENT']."-----".$conf['version']
+                )
+             );
+
+            // If we have a proxy url set up, use it to check with
+            if(isset($conf['http_proxy'])) {
+                if(preg_match('/http:\/\/([^:]+:?\d*)/',$conf['http_proxy'],$proxyhost)) {
+                    $stream_opts['http']['proxy'] = 'tcp://'.$proxyhost[1];
+                    $stream_opts['http']['request_fulluri'] = true;
+                }
+            }
+
+            // create a stream context
+            $context = stream_context_create($stream_opts);
+
             // use fsockopen to test that the connection works, if it does, open using fopen
             // for some reason the default_socket_timeout was not working properly.
-            $fsock = @fsockopen("tcp://{$onachkserver}", 80, $errNo, $errString, 2);
-            if ($fsock) {
+            //$fsock = @fsockopen("tcp://{$onachkserver}", 80, $errNo, $errString, 2);
+            //if ($fsock) {
                 $old = @ini_set('default_socket_timeout', 2);
-                $file = @fopen("http://{$onachkserver}/check_version.php", "r");
+                $file = @fopen($ona_check_version_url, 'r', false, $context);
                 @ini_set('default_socket_timeout', $old);
-            }
+            //}
         }
 
         $onaver = "Unable to determine";
