d50276df3d691b7c8b058deab9a5a4c366f74287
opennetadmin <matt@opennetadmin.com>
2011-08-24 17:45:22 +0000

    MP: adding hostname, onahostname, domain, onadomain as replacable options in host_actions

--- www/workspace_plugins/builtin/host_actions/config.inc.php
+++ www/workspace_plugins/builtin/host_actions/config.inc.php
@@ -3,14 +3,20 @@
 // You should copy this file to www/local/config/host_actions.config.php and make changes there
 //
 // This section defines host actions. If you leave the url blank it will not show the option in the list
-// You can use %fqdn, %ip, %loc or %ca[custom_attr] as substitutions in the url for the host being displayed
+// You can use %fqdn, %ip, %loc, %hostname, %onahostname, %domain, %onadomain, or %ca[custom_attr]
+// as substitutions in the url for the host being displayed.
 // You can specify a tooltip title for the option, otherwise it defaults to the hostaction name "Telnet" "Splunk" etc
-// These will be listed in the order specified here.
+//
 // %loc = the "reference" value from the hosts location
 // %fqdn = the fully qualified primary name of this host
 // %ip = the IP address of the primary interface for this host
-// %ca[custom_attr] will be substituted by the value of the Custom Attribute "custom_attr". If the host doesn't have one,
-// it will be substituted by the value of the system configuration "default_custom_attr".
+// %ca[custom_attr] = will be substituted by the value of the Custom Attribute "custom_attr". If the host doesn't have one,
+//                    it will be substituted by the value of the system configuration "default_custom_attr".
+// %hostname = the first octet portion of the fqdn.
+// %onahostname = the host name portion as stored in ONA, for when you have 'server1.loc' 'example.com'
+// %domain = everything but the first octet portion of the fqdn.
+// %onadomain = the domain portion as stored in ONA, for when you have 'server1.loc' 'example.com', gives you example.com
+
 
 //EXAMPLE:$conf[$modulename]['LINK_TITLE']['url'] = "http://something";
 $conf[$modulename]['Splunk']['url'] = "https://splunk.example.com:8001/?events/?eventspage=1&num=10&q=%fqdn";
