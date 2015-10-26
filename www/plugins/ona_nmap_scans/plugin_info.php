ad881d608d2ddac4a44cfd2c400f970910c471f2
Matt Pascoe <matt@opennetadmin.com>
2015-10-26 10:15:49 -0700

    minor fixups and new version info for nmap_scan plugin

--- www/plugins/ona_nmap_scans/plugin_info.php
+++ www/plugins/ona_nmap_scans/plugin_info.php
@@ -4,8 +4,7 @@ Your plugin must have a short description and a version number
 */
 
 $plugin_description = 'Compare NMAP results to data stored in ONA.';
-$plugin_version = '1.0';
-$plugin_help_url = 'http://opennetadmin.com/docs/ona_nmap_scans';
-
+$plugin_version = '1.1';
+$plugin_help_url = 'https://github.com/opennetadmin/ona/wiki/Plugin-ona-nmap-scans';
 
 ?>
